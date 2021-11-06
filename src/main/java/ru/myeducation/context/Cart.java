8102026af6b81abb8e03e7f0f80e58df03afc0d1
Ivan <ivan_semenov@bk.ru>
2021-11-07 01:52:00 +0300

    add application logic

--- src/main/java/ru/myeducation/context/Cart.java
+++ src/main/java/ru/myeducation/context/Cart.java
@@ -4,26 +4,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 @Scope("prototype")
 public class Cart {
     ProductRepository productRepository;
+    ProductService productService;
 
     @Autowired
     public void setProductRepository(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
 
-    List<Product> products;
+    List<Product> products = new ArrayList<>();
 
     public void addProductById(Long id){
-        products.add(productRepository.getProducts().stream().filter(p-> p.getId()==id).findFirst().orElseThrow(()->new RuntimeException("Нет такого продукта")));
+        if (productRepository.findById(id)!=null){
+            products.add(productRepository.findById(id));
+        }else {
+            System.out.println("Продукт отсутствует");
+        }
     }
 
     public void removeProductById(Long id){
         products.remove(productRepository.getProducts().stream().filter(p-> p.getId()==id).findFirst().orElseThrow(()->new RuntimeException("Нет такого продукта")));
     }
 
+    public void showCartContent(){
+        products.stream().forEach(System.out::println);
+    }
 }
--- src/main/java/ru/myeducation/context/MainApp.java
+++ src/main/java/ru/myeducation/context/MainApp.java
@@ -3,12 +3,49 @@ package ru.myeducation.context;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MainApp {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =  new AnnotationConfigApplicationContext("ru.myeducation.context");
         OrderService orderService = context.getBean(OrderService.class);
-        orderService.createOrderFromProduct(1L);
+        Cart cart = context.getBean(Cart.class);
+        boolean isActive = true;
 
-        context.close();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Добавить товар в корзину. Введите:/add productId");
+        System.out.println("Удалить товар из корзины. Введите: /del productId");
+        System.out.println("Показать содержимое корзины. Введите: /show cart");
+        System.out.println("Завершить работу. Введите: /exit");
+
+        while (isActive){
+            System.out.println("ожидаю ввода команды...");
+            String line = scanner.nextLine();
+            String[]tokens = line.split(" ");
+            Arrays.stream(tokens).forEach(System.out::println);
+            switch (tokens[0]){
+                case "/add":
+                    cart.addProductById(Long.parseLong(tokens[1]));
+                    System.out.println("Продукт: [" + cart.productService.getTitleById(Long.parseLong(tokens[1])) + "] добавлен в корзину");
+                    break;
+                case "/del":
+                    cart.removeProductById(Long.parseLong(tokens[1]));
+                    System.out.println("Продукт: [" + cart.productService.getTitleById(Long.parseLong(tokens[1])) + "] удален из корзины");
+                    break;
+                case "/show":
+                    cart.showCartContent();
+                    break;
+                case "/exit":
+                    isActive = false;
+                    break;
+                default:
+                    System.out.println("команда введена некорректно ");
+            }
+
+
+        }
+
+        //context.close();
     }
 }
