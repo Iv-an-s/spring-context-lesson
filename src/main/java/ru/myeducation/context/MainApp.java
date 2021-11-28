29ee21bbac537400755d3c3bd3d6514f7d5cee90
Ivan <ivan_semenov@bk.ru>
2021-11-29 01:15:52 +0300

    preparing for hw6

--- src/main/java/ru/myeducation/context/MainApp.java
+++ src/main/java/ru/myeducation/context/MainApp.java
@@ -2,43 +2,8 @@ package ru.myeducation.context;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.Scanner;
-
 public class MainApp {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("ru.myeducation.context");
-        Cart cart = context.getBean(Cart.class);
-        boolean isActive = true;
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Добавить товар в корзину. Введите:/add productId");
-        System.out.println("Удалить товар из корзины. Введите: /del productId");
-        System.out.println("Показать содержимое корзины. Введите: /show cart");
-        System.out.println("Завершить работу. Введите: /exit");
-
-        while (isActive) {
-            System.out.println("ожидаю ввода команды...");
-            String line = scanner.nextLine();
-            String[] tokens = line.split(" ");
-            switch (tokens[0]) {
-                case "/add":
-                    cart.addProductById(Long.parseLong(tokens[1]));
-                    break;
-                case "/del":
-                    cart.removeProductById(Long.parseLong(tokens[1]));
-                    break;
-                case "/show":
-                    System.out.println("Содержимое корзины:");
-                    cart.showCartContent();
-                    break;
-                case "/exit":
-                    System.out.println("Завершаю работу...");
-                    isActive = false;
-                    break;
-                default:
-                    System.out.println("команда введена некорректно ");
-            }
-        }
-        context.close();
     }
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,17 @@
+DROP TABLE products IF EXISTS;
+CREATE TABLE IF NOT EXISTS products(id bigserial, title VARCHAR(255), price INTEGER, PRIMARY KEY (id));
+INSERT INTO products (title, price) VALUES ('Milk', 10), ('Bread', 20), ('Apple', 30);
+
+DROP TABLE customers IF EXISTS;
+CREATE TABLE IF NOT EXISTS customers(id bigserial, name VARCHAR(255), PRIMARY KEY (id));
+INSERT INTO customers (name) VALUES ('Bob'), ('John'), ('Jack');
+
+DROP TABLE IF EXISTS customers_products CASCADE;
+CREATE TABLE IF NOT EXISTS customers_products (customer_id INTEGER, product_id INTEGER, FOREIGN KEY (customer_id) REFERENCES customers (id), FOREIGN KEY (product_id) REFERENCES products (id));
+INSERT INTO customers_products (customer_id, product_id) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(1, 2),
+(1, 3),
+(3, 3);
